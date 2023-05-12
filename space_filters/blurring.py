dbff4aa17d832d143e4638fb8611b87f0db8346a
Mirza Hodžić <mirza.hodzic6602@gmail.com>
2023-05-12 07:01:08 -0700

    Add noise addition functions
    Signed-off-by: Mirza Hodžić <mirza.hodzic6602@gmail.com>

--- space_filters/blurring.py
+++ space_filters/blurring.py
@@ -1,10 +1,55 @@
 import cv2 as cv
 import os
+import numpy as np
+
+def noisy(noise_typ,image):
+   if noise_typ == "gauss":
+      row,col,ch= image.shape
+      mean = 0
+      var = 0.1
+      sigma = var**0.05
+      gauss = np.random.normal(mean,sigma,(row,col,ch))
+      gauss = gauss.reshape(row,col,ch)
+      noisy = image + gauss
+      return noisy
+   elif noise_typ == "s&p":
+      row,col,ch = image.shape
+      s_vs_p = 0.5
+      amount = 0.004
+      out = np.copy(image)
+      # Salt mode
+      num_salt = np.ceil(amount * image.size * s_vs_p)
+      coords = [np.random.randint(0, i - 1, int(num_salt))
+              for i in image.shape]
+      out[coords] = 1
+
+      # Pepper mode
+      num_pepper = np.ceil(amount* image.size * (1. - s_vs_p))
+      coords = [np.random.randint(0, i - 1, int(num_pepper))
+              for i in image.shape]
+      out[coords] = 0
+      return out
+   elif noise_typ == "poisson":
+      vals = len(np.unique(image))
+      vals = 2 ** np.ceil(np.log2(vals))
+      noisy = np.random.poisson(image * vals) / float(vals)
+      return noisy
+   elif noise_typ =="speckle":
+      row,col,ch = image.shape
+      gauss = np.random.randn(row,col,ch)
+      gauss = gauss.reshape(row,col,ch)        
+      noisy = image + image * gauss
+      return noisy
 
 def main():
-    path = os.path.join('..', os.getcwd(), 'images', 'm1a2_abrams_l5.jpg')
+    path = os.path.join('..', os.getcwd(), 'images', 'lena.jpg')
     img = cv.imread(path)
+    noisy_gauss_img = noisy("gauss", img)
+    noisy_salt_pepper_img = noisy("s&p", img)
+
     cv.imshow('Original', img)
+    cv.imshow('Gaussian noise', noisy_gauss_img)
+    cv.imshow('Salt and pepper noise', noisy_salt_pepper_img)
 
     # Avergaing filter
     average = cv.blur(img, (3,3))
@@ -15,7 +60,7 @@ def main():
     cv.imshow('Gaussian blur', gaussian)
 
     # Median blur, good for salt and pepper noise
-    median = cv.medianBlur(img, 3)
+    median = cv.medianBlur(noisy_salt_pepper_img, 3)
     cv.imshow('Median', median)
 
     # Bilater blurring, applies blurring but retains edges in the image
