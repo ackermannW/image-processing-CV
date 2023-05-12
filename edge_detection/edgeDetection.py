f3900397636e84f143275b408beeae818dad5716
Mirza Hodžić <mirza.hodzic6602@gmail.com>
2023-05-12 14:42:25 -0700

    Add image sharpening
    Signed-off-by: Mirza Hodžić <mirza.hodzic6602@gmail.com>

--- edge_detection/edgeDetection.py
+++ edge_detection/edgeDetection.py
@@ -19,6 +19,7 @@ def main():
     sobelx = cv.Sobel(gray, cv.CV_64F, 1, 0 )
     sobely = cv.Sobel(gray, cv.CV_64F, 0, 1)
     combined_sobel = cv.bitwise_or(sobelx, sobely)
+    combined_sobel = np.uint8(np.absolute(combined_sobel))
 
     cv.imshow('Sobelx', sobelx)
     cv.imshow('Sobely', sobely)
@@ -27,6 +28,16 @@ def main():
     canny = cv.Canny(gray, 150,175)
     cv.imshow('Canny', canny)
 
+    cv.waitKey(0)
+
+    sharpened_lap = cv.subtract(gray, lap)
+    sharpened_sobel = cv.subtract(gray, combined_sobel.astype(np.uint8))
+    sharpened_canny = cv.subtract(gray, canny)
+
+    cv.imshow("Sharpening using Laplacian", sharpened_lap)
+    cv.imshow("Sharpening using Sobel", sharpened_sobel)
+    cv.imshow("Sharpening using Canny", sharpened_canny)
+    
     cv.waitKey(0)
 
 if __name__ == '__main__':
