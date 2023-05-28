58184867767b579f2fc11a998c49ac4b7e0138f3
Mirza Hodžić <mirza.hodzic6602@gmail.com>
2023-05-28 11:06:25 -0800

    Add Otsu example
    Signed-off-by: Mirza Hodžić <mirza.hodzic6602@gmail.com>

---
+++ histograms_and_thresholding/binarization.py
@@ -0,0 +1,34 @@
+import cv2 as cv
+import numpy as np
+from matplotlib import pyplot as plt
+import os
+
+def main():
+    path = os.path.join('..', os.getcwd(), 'images', 'lena.jpg')
+    img = cv.imread(path, cv.IMREAD_GRAYSCALE)
+    assert img is not None, "file could not be read, check with os.path.exists()"
+    # global thresholding
+    ret1,th1 = cv.threshold(img,127,255,cv.THRESH_BINARY)
+    # Otsu's thresholding
+    ret2,th2 = cv.threshold(img,0,255,cv.THRESH_BINARY+cv.THRESH_OTSU)
+    # Otsu's thresholding after Gaussian filtering
+    blur = cv.GaussianBlur(img,(5,5),0)
+    ret3,th3 = cv.threshold(blur,0,255,cv.THRESH_BINARY+cv.THRESH_OTSU)
+    # plot all the images and their histograms
+    images = [img, 0, th1,
+            img, 0, th2,
+            blur, 0, th3]
+    titles = ['Original Noisy Image','Histogram','Global Thresholding (v=127)',
+            'Original Noisy Image','Histogram',"Otsu's Thresholding",
+            'Gaussian filtered Image','Histogram',"Otsu's Thresholding"]
+    for i in range(3):
+        plt.subplot(3,3,i*3+1),plt.imshow(images[i*3],'gray')
+        plt.title(titles[i*3]), plt.xticks([]), plt.yticks([])
+        plt.subplot(3,3,i*3+2),plt.hist(images[i*3].ravel(),256)
+        plt.title(titles[i*3+1]), plt.xticks([]), plt.yticks([])
+        plt.subplot(3,3,i*3+3),plt.imshow(images[i*3+2],'gray')
+        plt.title(titles[i*3+2]), plt.xticks([]), plt.yticks([])
+    plt.show()
+
+if __name__ == "__main__":
+    main()
