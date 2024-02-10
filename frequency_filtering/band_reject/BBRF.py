58ec8a0a792a1bbccc9ec59f6a856b6c9d6ca657
Mirza Hodžić <mirza.hodzic6602@gmail.com>
2024-02-10 14:51:06 +0100

    Implement examples for band reject filters
    Signed-off-by: Mirza Hodžić <mirza.hodzic6602@gmail.com>

---
+++ frequency_filtering/band_reject/BBRF.py
@@ -0,0 +1,56 @@
+import os
+import cv2 as cv
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
+
+def main():
+    script_path = os.path.abspath(sys.argv[0])
+    path = os.path.abspath(os.path.join(os.path.dirname(script_path), '..', '..', 'images', 'lena.jpg'))
+
+    imgGray  = cv.imread(path, flags=0) # flags=0 to import as grrayscale
+
+    f = np.fft.fft2(imgGray)
+    f_shift = np.fft.fftshift(f)
+
+    M,N = f.shape
+    H = np.zeros((M,N), dtype=np.float32)
+    W = 300
+    C0 = 90
+    H = np.zeros(shape=f.shape, dtype = np.float32)
+    for u in range(M):
+        for v in range(N):
+            D = np.sqrt((u-M/2)**2 + (v-N/2)**2)
+            H[u,v] = 1/((1+D*W/(D**2-C0**2)**2))
+    
+    G_shift = f_shift * H
+    G = np.fft.ifftshift(G_shift)
+    
+    g = np.abs(np.fft.ifft2(G))
+
+    fig, axs = plt.subplots(2,3)
+    axs[0,0].imshow(imgGray, cmap='gray')
+    axs[0,0].set_title('Original image')
+
+    axs[0,1].imshow(np.log1p(np.abs(f_shift)), cmap='gray')
+    axs[0,1].set_title('Magnitude spectrum')
+
+    axs[0,2].imshow(H, cmap='gray')
+    axs[0,2].set_title('Butterworth band reject')
+
+    axs[1,0].imshow(np.log1p(np.abs(G_shift)), cmap='gray')
+    axs[1,0].set_title('Centered filtered FT')
+
+    axs[1,1].imshow(np.log1p(np.abs(G)), cmap='gray')
+    axs[1,1].set_title('Shifted filtered FT')
+
+    axs[1,2].imshow(g, cmap='gray')
+    axs[1,2].set_title('Restored image')
+
+    fig.show()
+
+    plt.tight_layout()
+
+    plt.show()
+if __name__ == '__main__':
+    main()
