fe65dea5676f7bf9846d6f82aa4d0353802730ba
Osman Hadzic <osman.hadzic@secomind.com>
2024-02-14 10:39:56 +0100

    Refactor plotting images on one plot
    A script to display images across multiple plots. Refactoring from
    multiple plots to one plot for improved visibility.
    
    Signed-off-by: Osman Hadzic <osman.hadzic@secomind.com>

--- image_segmentation/coins.py
+++ image_segmentation/coins.py
@@ -1,58 +1,67 @@
 import os
 import numpy as np
 import cv2 as cv
-from matplotlib import pyplot as plt
 import sys
+import matplotlib.pyplot as plt
 
 def main():
     script_path = os.path.abspath(sys.argv[0])
     path = os.path.abspath(os.path.join(os.path.dirname(script_path), '..', 'images', 'coins.jpg'))
-    img  = cv.imread(path)
-    gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
-    ret, thresh = cv.threshold(gray,0,255,cv.THRESH_BINARY_INV+cv.THRESH_OTSU)
-    cv.imshow('binary image', thresh)
-    cv.waitKey(0)
+    img = cv.imread(path)
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    ret, thresh = cv.threshold(gray, 0, 255, cv.THRESH_BINARY_INV + cv.THRESH_OTSU)
 
-    # noise removal
-    # we use opening to remove smal white noises from the image
-    kernel = np.ones((3,3),np.uint8)
-    opening = cv.morphologyEx(thresh,cv.MORPH_OPEN,kernel, iterations = 2)
+    # Noise removal
+    kernel = np.ones((3, 3), np.uint8)
+    opening = cv.morphologyEx(thresh, cv.MORPH_OPEN, kernel, iterations=2)
 
-    cv.imshow('opened image', opening)
-    cv.waitKey(0)
-
-    # sure background area
-    sure_bg = cv.dilate(opening,kernel,iterations=3)
+    # Sure background area
+    sure_bg = cv.dilate(opening, kernel, iterations=3)
 
     # Finding sure foreground area
-    dist_transform = cv.distanceTransform(opening,cv.DIST_L2,5)
-    
-    cv.imshow('Distance transform', dist_transform)
-    cv.waitKey(0)
-    
-    ret, sure_fg = cv.threshold(dist_transform,0.7*dist_transform.max(),255,0)
-    # Finding unknown region
+    dist_transform = cv.distanceTransform(opening, cv.DIST_L2, 5)
+    ret, sure_fg = cv.threshold(dist_transform, 0.7 * dist_transform.max(), 255, 0)
     sure_fg = np.uint8(sure_fg)
-    unknown = cv.subtract(sure_bg,sure_fg)
-
-    cv.imshow('Sure foreground', sure_fg)
-    cv.waitKey(0)
-
-    cv.imshow('Unknown', unknown)
-    cv.waitKey(0)
+    unknown = cv.subtract(sure_bg, sure_fg)
 
     # Marker labelling
     ret, markers = cv.connectedComponents(sure_fg)
-    # Add one to all labels so that sure background is not 0, but 1
-    markers = markers+1
+    markers = markers + 1
+    markers[unknown == 255] = 0
 
-    # Now, mark the region of unknown with zero
-    markers[unknown==255] = 0
-    
-    markers = cv.watershed(img,markers)
-    img[markers == -1] = [255,0,0]
-    
-    cv.imshow('segmentated image',img)
-    cv.waitKey(0)
+    # Apply watershed algorithm
+    markers = cv.watershed(img, markers)
+    img[markers == -1] = [255, 0, 0]
+
+    # Plotting all images in one screen
+    fig, axs = plt.subplots(2, 3, figsize=(12, 8))
+
+    axs[0, 0].imshow(cv.cvtColor(img, cv.COLOR_BGR2RGB))
+    axs[0, 0].set_title('Segmented Image')
+    axs[0, 0].axis('off')
+
+    axs[0, 1].imshow(thresh, cmap='gray')
+    axs[0, 1].set_title('Binary Image')
+    axs[0, 1].axis('off')
+
+    axs[0, 2].imshow(opening, cmap='gray')
+    axs[0, 2].set_title('Opened Image')
+    axs[0, 2].axis('off')
+
+    axs[1, 0].imshow(sure_bg, cmap='gray')
+    axs[1, 0].set_title('Sure Background')
+    axs[1, 0].axis('off')
+
+    axs[1, 1].imshow(sure_fg, cmap='gray')
+    axs[1, 1].set_title('Sure Foreground')
+    axs[1, 1].axis('off')
+
+    axs[1, 2].imshow(unknown, cmap='gray')
+    axs[1, 2].set_title('Unknown')
+    axs[1, 2].axis('off')
+
+    plt.tight_layout()
+    plt.show()
+
 if __name__ == '__main__':
     main()
