c1973e90bef8397baf651addb2267ba4afef7ecf
Mirza <mirza.hodzic6602@gmail.com>
2024-02-16 09:01:18 +0100

    test 2
    Signed-off-by: Mirza <mirza.hodzic6602@gmail.com>

--- morphological_processing/morphological_skeleton.py
+++ morphological_processing/morphological_skeleton.py
@@ -1,4 +1,3 @@
-# Import the necessary libraries
 import cv2 as cv
 import os
 import numpy as np
@@ -9,17 +8,21 @@ def main():
     script_path = os.path.abspath(sys.argv[0])
     path = os.path.abspath(os.path.join(os.path.dirname(script_path), '..', 'images', 'coins.jpg'))
     img  = cv.imread(path, flags=0) # flag 0 for grayscale image
-    # Read the image as a grayscale image
-    cv.imshow('image', img)
-    cv.waitKey(0)
     
     # Threshold the image
-    ret,img = cv.threshold(img, 127, 255, cv.THRESH_BINARY)
-    #img = cv.adaptiveThreshold(img, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 11, 3) 
-    cv.imshow('binarized', img)
-    cv.waitKey(0)
+    _, bin_img = cv.threshold(img, 127, 255, cv.THRESH_BINARY)
+    
+    plt.subplot(1,3,1)
+    plt.imshow(img, cmap='gray')
+    plt.title('Original image')
+    plt.axis('off')
+
+    plt.subplot(1,3,2)
+    plt.imshow(bin_img, cmap='gray')
+    plt.title('Binarized image')
+    plt.axis('off')
+
     # Step 1: Create an empty skeleton
-    size = np.size(img)
     skel = np.zeros(img.shape, np.uint8)
 
     # Get a Cross Shaped Kernel
@@ -27,21 +30,23 @@ def main():
     # Repeat steps 2-4
     while True:
         #Step 2: Open the image
-        open = cv.morphologyEx(img, cv.MORPH_OPEN, element)
+        open = cv.morphologyEx(bin_img, cv.MORPH_OPEN, element)
         #Step 3: Substract open from the original image
-        temp = cv.subtract(img, open)
+        temp = cv.subtract(bin_img, open)
         #Step 4: Erode the original image and refine the skeleton
-        eroded = cv.erode(img, element)
+        eroded = cv.erode(bin_img, element)
         skel = cv.bitwise_or(skel,temp)
-        img = eroded.copy()
+        bin_img = eroded.copy()
         # Step 5: If there are no white pixels left ie.. the image has been completely eroded, quit the loop
-        if cv.countNonZero(img)==0:
+        if cv.countNonZero(bin_img)==0:
             break
 
-    # Displaying the final skeleton
-    cv.imshow("Skeleton",skel)
-    cv.waitKey(0)
-    cv.destroyAllWindows()
+    plt.subplot(1,3,3)
+    plt.imshow(skel, cmap='gray')
+    plt.title('Skeleton')
+    plt.axis('off')
+
+    plt.show()
 
 if __name__ == "__main__":
     main()
