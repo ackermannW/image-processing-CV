be4342a6eb1c845ff8e7b24acc274e61dfea54f7
Mirza <mirza.hodzic6602@gmail.com>
2024-05-11 10:49:07 +0200

    Fix gaussian noise addition to the example
    Signed-off-by: Mirza <mirza.hodzic6602@gmail.com>

--- space_filters/blurring.py
+++ space_filters/blurring.py
@@ -5,31 +5,27 @@ import sys
 
 def noisy(noise_typ,image):
    if noise_typ == "gauss":
-      row,col,ch= image.shape
       mean = 0
-      var = 0.1
-      sigma = var**0.05
-      gauss = np.random.normal(mean,sigma,(row,col,ch))
-      gauss = gauss.reshape(row,col,ch)
-      noisy = image + gauss
+      sigma = 5
+      gauss = np.random.normal(mean,sigma,image.shape).astype(np.uint8)
+      noisy = cv.add(image, gauss)
       return noisy
    elif noise_typ == "s&p":
-      row,col,ch = image.shape
-      s_vs_p = 0.5
-      amount = 0.004
-      out = np.copy(image)
-      # Salt mode
-      num_salt = np.ceil(amount * image.size * s_vs_p)
-      coords = [np.random.randint(0, i - 1, int(num_salt))
-              for i in image.shape]
-      out[coords] = 1
-
-      # Pepper mode
-      num_pepper = np.ceil(amount* image.size * (1. - s_vs_p))
-      coords = [np.random.randint(0, i - 1, int(num_pepper))
-              for i in image.shape]
-      out[coords] = 0
-      return out
+      pepper = 0.05
+      salt = 1 - pepper
+      M,N = image.shape
+      noisy = np.zeros((M,N), dtype='uint8')
+      for i in range(M):
+        for j in range(N):
+            rdn = np.random.random()
+            if rdn < pepper:
+                noisy[i][j] = 0
+            elif rdn > salt:
+                noisy[i][j] = 255
+            else:
+                noisy[i][j] = image[i][j]
+       
+      return noisy
    elif noise_typ == "poisson":
       vals = len(np.unique(image))
       vals = 2 ** np.ceil(np.log2(vals))
@@ -45,7 +41,7 @@ def noisy(noise_typ,image):
 def main():
     script_path = os.path.abspath(sys.argv[0])
     path = os.path.abspath(os.path.join(os.path.dirname(script_path), '..', 'images', 'lena.jpg'))
-    img = cv.imread(path)
+    img = cv.imread(path, 0)
     noisy_gauss_img = noisy("gauss", img)
     noisy_salt_pepper_img = noisy("s&p", img)
 
@@ -64,10 +60,6 @@ def main():
     # Median blur, good for salt and pepper noise
     median = cv.medianBlur(noisy_salt_pepper_img, 3)
     cv.imshow('Median', median)
-
-    # Bilater blurring, applies blurring but retains edges in the image
-    bilateral = cv.bilateralFilter(img, 10, 35, 25)
-    cv.imshow('Bilateral filter', bilateral)
 
     cv.waitKey(0)
 
